25f53862aa9da7b91cc379837691809694d21f13
Asgertor <asge1114@stud.kea.dk>
2022-11-25 09:14:16 +0100

    Reformatted code

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -4,41 +4,41 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Controller {
-
+    
     Database database;
     Filehandler filehandler;
-
-    public Controller(){
+    
+    public Controller() {
         database = new Database();
         filehandler = new Filehandler();
     }
-
-    public void createSwimmer(String name, String address, String phoneNumber, String mail, LocalDate birthday, boolean isCompetitor, boolean isStudent){
+    
+    public void createSwimmer(String name, String address, String phoneNumber, String mail, LocalDate birthday, boolean isCompetitor, boolean isStudent) {
         int memberID = database.getSwimmerList().size();
         Integer.parseInt(String.format("%03d", memberID));
-
+        
         Swimmer swimmer = new Swimmer(name, address, phoneNumber, mail, birthday, memberID, isCompetitor, isStudent);
         database.addSwimmerToDatabase(swimmer);
     }
-
-    public ArrayList<Swimmer> searchForMembers(String searchParameter){
+    
+    public ArrayList<Swimmer> searchForMembers(String searchParameter) {
         return database.searchForMembers(searchParameter);
     }
-
-    public ArrayList<Swimmer> searchForArchived(String searchParameter){
+    
+    public ArrayList<Swimmer> searchForArchived(String searchParameter) {
         return database.searchForArchived(searchParameter);
     }
-
-    public ArrayList<Swimmer> getSwimmerList(){
-            return database.getSwimmerList();
+    
+    public ArrayList<Swimmer> getSwimmerList() {
+        return database.getSwimmerList();
     }
-
-    public void overwriteSwimmerDatabase(){
+    
+    public void overwriteSwimmerDatabase() {
         filehandler.overwriteSwimmerDatabase(database.getSwimmerList());
     }
-
+    
     public void initialLoad() {
         database.setSwimmerDatabase(filehandler.retrieveSwimmerDatabase());
     }
-
+    
 }
